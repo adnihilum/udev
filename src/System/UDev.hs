dcda1d29da22b8157eac547e8363f5ff6080fd2e
adnihilum <b1er.new@gmail.com>
2019-09-23 00:11:37 +0500

    init stack project

--- src/System/UDev.hs
+++ src/System/UDev.hs
@@ -6,34 +6,32 @@
 --   Portability :  portable
 --
 module System.UDev
-       ( module System.UDev.Context
-       , module System.UDev.Device
-       , module System.UDev.Enumerate
-       , module System.UDev.HWDB
-       , module System.UDev.List
-       , module System.UDev.Monitor
-       , module System.UDev.Queue
-       , module System.UDev.Util
-       ) where
+    ( module System.UDev.Context
+    , module System.UDev.Device
+    , module System.UDev.Enumerate
+    , module System.UDev.HWDB
+    , module System.UDev.List
+    , module System.UDev.Monitor
+    , module System.UDev.Queue
+    , module System.UDev.Util) where
 
-import Data.Monoid
+import           System.UDev.Context
+import           System.UDev.Device
+import           System.UDev.Enumerate
+import           System.UDev.HWDB
+import           System.UDev.List
+import           System.UDev.Monitor
+import           System.UDev.Queue
+import           System.UDev.Util
 
-import System.UDev.Context
-import System.UDev.Device
-import System.UDev.Enumerate
-import System.UDev.HWDB
-import System.UDev.List
-import System.UDev.Monitor
-import System.UDev.Queue
-import System.UDev.Util
+type Devtype = ()
 
-
-type Devtype   = ()
-type Tag       = ()
+type Tag = ()
 
 data Filter = Filter (Maybe (Subsystem, Devtype)) (Maybe Tag)
 
+instance Semigroup Filter where
+  Filter sda ta <> Filter sdb tb = Filter (sda <> sdb) (ta <> tb)
+
 instance Monoid Filter where
-  mempty  = Filter Nothing Nothing
-  Filter sda ta `mappend` Filter sdb tb
-    = Filter (sda `mappend` sdb) (ta `mappend` tb)
+  mempty = Filter Nothing Nothing
